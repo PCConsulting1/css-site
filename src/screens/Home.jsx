3337d4aec649619042b0959e9591efd658bcc2b4
Cody <codyhenry527@gmail.com>
2023-03-30 12:05:08 -0400

    added lines to Home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
         <>
             <h1>Company Name</h1>
             <p>Add a few lines about company</p>
+            <p>Add 2 cards about payment plans</p>
         </>
     )
 }
